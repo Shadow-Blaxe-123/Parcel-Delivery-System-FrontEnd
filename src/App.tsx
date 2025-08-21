7cc699aeec4402e26cdfc1cf80d675d7db176151
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-08-21 15:21:05 +0600

    dark mode added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "./components/ModeToggle";
+
+function App() {
+  return (
+    <div>
+      <h1>This is App component.</h1>
+      <ModeToggle />
+    </div>
+  );
+}
+export default App;
